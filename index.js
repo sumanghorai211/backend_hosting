2673f9321c03b21bb1766d29a4cfddd7bb8f86f5
suman-ghorai <sumanghorai211@gmail.com>
2024-01-13 16:56:38 +0530

    comit with cors

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 require("dotenv").config();
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hi the app is working");
 });
+const data = [
+  {
+    name: "suman1",
+    class: "10",
+    subjects: "math",
+  },
+  {
+    name: "suman2",
+    class: "10",
+    subjects: "math",
+  },
+  {
+    name: "suman3",
+    class: "10",
+    subjects: "math",
+  },
+];
 app.get("/check", (req, res) => {
-  res.send("hi the app is working and checking is also successful");
+  res.send(data);
 });
 
 app.listen(process.env.PORT, (req, res) => {
