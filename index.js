49db6e8a4311ef06e45132f65f45fe2986c63371
suman-ghorai <sumanghorai211@gmail.com>
2024-01-13 15:59:42 +0530

    3rd changes

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 app.get("/", (req, res) => {
   res.send("hi the app is working");
 });
+app.get("/check", (req, res) => {
+  res.send("hi the app is working and checking is also successful");
+});
+
 app.listen(process.env.PORT, (req, res) => {
   console.log(`app is listening on port no ${process.env.PORT}`);
 });
